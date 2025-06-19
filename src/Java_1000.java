d29c7edb2aa91d5ea7426ce5581507e286454118
cfcromn <s25058@gsm.hs.kr>
2025-06-19 17:47:37 +0900

    coding-test

---
+++ src/Java_1000.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Java_1000 {
+
+    public static void main(String[] args) {
+
+        Scanner sum = new Scanner(System.in);
+
+        int a = sum.nextInt();
+        int b = sum.nextInt();
+
+        System.out.println(a + b);
+    }
+}

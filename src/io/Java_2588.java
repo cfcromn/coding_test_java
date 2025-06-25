b9fb4168d08131d3670c7a06250f3283dab85b9d
cfcromn <s25058@gsm.hs.kr>
2025-06-25 12:20:12 +0900

    coding-test

---
+++ src/io/Java_2588.java
@@ -0,0 +1,18 @@
+package io;
+
+import java.util.Scanner;
+
+public class Java_2588 {
+
+    public static void main(String[] args) {
+        Scanner number = new Scanner(System.in);
+        int num1 = number.nextInt();
+        int num2 = number.nextInt();
+
+
+        System.out.println(num1 * (num2 % 10));
+        System.out.println(num1 * ((num2 / 10) % 10));
+        System.out.println(num1 * ((num2 / 100) % 10));
+        System.out.println(num1 * num2);
+    }
+}

4527e3a47d4d2a558af11f354498711799655cfd
cfcromn <s25058@gsm.hs.kr>
2025-06-27 22:00:14 +0900

    coding-test

---
+++ src/io/Java_11382.java
@@ -0,0 +1,16 @@
+package io;
+
+import java.util.Scanner;
+
+public class Java_11382 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+        long sum = a + b + c;
+
+        System.out.println(sum);
+    }
+}

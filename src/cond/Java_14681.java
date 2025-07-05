195a6f69714ff85f72babbf914b94f0aff1056a5
cfcromn <s25058@gsm.hs.kr>
2025-07-05 17:45:03 +0900

    coding-test

---
+++ src/cond/Java_14681.java
@@ -0,0 +1,24 @@
+package cond;
+
+import java.util.Scanner;
+
+public class Java_14681 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int xCoordinate = scanner.nextInt();
+        int yCoordinate = scanner.nextInt();
+
+        if (xCoordinate > 0 && yCoordinate > 0) {
+            System.out.println("1");
+        } else if (xCoordinate < 0 && yCoordinate > 0) {
+            System.out.println("2");
+        } else if (xCoordinate < 0 && yCoordinate < 0) {
+            System.out.println("3");
+        } else if (xCoordinate > 0 && yCoordinate < 0) {
+            System.out.println("4");
+        }
+    }
+}
+

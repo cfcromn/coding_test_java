79066703597318b5fccb0d94f29cb8731452a90c
cfcromn <s25058@gsm.hs.kr>
2025-07-07 09:55:25 +0900

    coding-test

---
+++ src/cond/Java_2525.java
@@ -0,0 +1,22 @@
+package cond;
+
+import java.util.Scanner;
+
+public class Java_2525 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        int t = sc.nextInt();
+        int totalTime = h * 60 + m + t;
+
+        if(totalTime >= 24*60) {
+            totalTime = totalTime - 24*60;
+            System.out.println(totalTime / 60 + " "+ totalTime % 60);
+        } else {
+            System.out.println(totalTime / 60 + " " + totalTime % 60);
+        }
+    }
+}

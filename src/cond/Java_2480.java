4364fb25b4351500d63c3b53f2cda23df7076d99
cfcromn <s25058@gsm.hs.kr>
2025-07-07 10:07:27 +0900

    coding-test

--- src/cond/Java_2480.java
+++ src/cond/Java_2480.java
@@ -3,25 +3,26 @@ package cond;
 import java.util.Scanner;
 
 public class Java_2480 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int first = sc.nextInt();
-        int second = sc.nextInt();
-        int third = sc.nextInt();
 
-        if (first == second && first == third) {
-            System.out.println(10000 + first * 1000);
-        } else if (first != second && first != third && second != third) {
-            System.out.println(((first > second)? (first > third)? first:third : (second > third)? second:third) * 100);
-        } else if (first != second && second == third || first == second && second != third || first != second && first == third) {
-            if (second == third) {
-                System.out.println(second * 100 + 1000);
-            } else if (first == third) {
-                System.out.println(first * 100 + 1000);
-            } else if (second == first) {
-                System.out.println(second * 100 + 1000);
-            }
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int prize = 0;
+
+        if (a == b && b == c) {
+            prize = 10000 + a * 1000;
+        } else if (a == b || a == c) {
+            prize = 1000 + a * 100;
+        } else if (b == c) {
+            prize = 1000 + b * 100;
+        } else {
+            int max = Math.max(a, Math.max(b, c));
+            prize = max * 100;
         }
+
+        System.out.println(prize);
     }
 }
+

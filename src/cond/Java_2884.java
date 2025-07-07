747e1fd86d084c5f0781fdc6942632ec711a3394
cfcromn <s25058@gsm.hs.kr>
2025-07-07 09:37:17 +0900

    coding-test

---
+++ src/cond/Java_2884.java
@@ -0,0 +1,21 @@
+package cond;
+
+import java.util.Scanner;
+
+public class Java_2884 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        int m = sc.nextInt();
+        int time = t * 60 + m - 45;
+
+        if (t <24 && m < 60) {
+            if (time < 0) {
+                time += 24 * 60;
+            }
+            System.out.println(time/60 + " " + time % 60);
+        }
+    }
+}

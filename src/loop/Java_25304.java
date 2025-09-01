de8bceab66b5b7db71b8a514a7235ded104c7341
박하민 <hamin090403@gmail.com>
2025-09-01 20:41:17 +0900

    coding-test

---
+++ src/loop/Java_25304.java
@@ -0,0 +1,24 @@
+package loop;
+
+import java.util.Scanner;
+
+public class Java_25304 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalPrice = sc.nextInt();
+        int count = sc.nextInt();
+        int sumPrice = 0;
+
+        for (int i = 0; i < count; i++) {
+            int price = sc.nextInt();
+            int quantity = sc.nextInt();
+            sumPrice += price * quantity;
+        }
+        if (sumPrice == totalPrice) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
+

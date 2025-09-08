8ecec8127eeae3671654d8af957228f834cec8b6
박하민 <hamin090403@gmail.com>
2025-09-08 15:38:54 +0900

    coding-test

---
+++ src/baekjoon/Java_2439.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Java_2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (n-j >= i) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

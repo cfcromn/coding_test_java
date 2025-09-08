ce64e78dcf13dfbbd9f56a94e6d83fa697b5e064
박하민 <hamin090403@gmail.com>
2025-09-08 15:58:20 +0900

    coding-test

---
+++ src/baekjoon/Java_15552.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Java_15552 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            sb.append(A + B).append('\n');
+        }
+
+        System.out.print(sb.toString());
+    }
+}

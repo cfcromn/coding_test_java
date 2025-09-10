d283651d91df573df2343639dbc076ba41c8682f
박하민 <hamin090403@gmail.com>
2025-09-10 13:47:43 +0900

    coding-test

---
+++ src/baekjoon/Java_10818.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Java_10818 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        int min = arr[0];
+
+        for (int i : arr) {
+            if (i > max) {
+                max = i;
+            }
+            if (i < min) {
+                min = i;
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}

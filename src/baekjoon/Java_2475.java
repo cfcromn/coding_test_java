b1384ec471d5b7a6a17704a218463e22d6d8ced7
박하민 <hamin090403@gmail.com>
2025-09-11 14:50:26 +0900

    coding-test

---
+++ src/baekjoon/Java_2475.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Java_2475 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[5];
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i] * arr[i];
+        }
+        System.out.println(sum%10);
+    }
+}
